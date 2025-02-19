b9d6645963409732c3351cf1d8c583eb6be2b70e
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-19 11:30:31 +0900

    수정

--- chapter4_real/Book3.java
+++ chapter4_real/Book3.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Book3 {
 	String title;
 	String author;
-	
-	
+
 	public Book3(String title, String author) {
 		super();
 		this.title = title;
@@ -15,19 +14,23 @@ public class Book3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Book3 [] book = new Book3[2];
+		Book3[] book = new Book3[2];
 		Scanner scanner = new Scanner(System.in);
-		for (int i = 0; i<book.length; i++) {
+		for (int i = 0; i < book.length; i++) {
+
 			System.out.print("제목>>");
 			String title = scanner.nextLine();
+
 			System.out.print("저자>>");
 			String author = scanner.nextLine();
+
 			book[i] = new Book3(title, author);
 		}
-		for (int i = 0; i<book.length; i++) {
-			System.out.println("제목 "+book[i].title+" 저자 "+book[i].author);
-			
+		for (int i = 0; i < book.length; i++) {
+			System.out.println("제목 " + book[i].title + " 저자 " + book[i].author);
+
 		}
+		scanner.close();
 	}
 
 }
