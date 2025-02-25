a61f82457a29bae36fea105d1afca5eacb46011e
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-25 17:37:09 +0900

    다수 클래스 계산기

---
+++ question08/Calc.java
@@ -0,0 +1,103 @@
+package Java.question08;
+
+import java.util.Scanner;
+
+class Add {
+	private int a;
+	private int b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a + b;
+	}
+
+}
+
+class Sub {
+	private int a;
+	private int b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a - b;
+	}
+
+}
+
+class Mul {
+	private int a;
+	private int b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a * b;
+	}
+
+}
+
+class Div {
+	private int a;
+	private int b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int calculate() {
+		return a / b;
+	}
+
+}
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 정수와 연산자를 입력하시오>> ");
+		
+		int a = 0, b = 0;
+		String s = "";
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		s = sc.next();
+		
+		switch(s) {
+		case "+":
+			Add add = new Add();
+			add.setValue(a, b);
+			System.out.println(add.calculate());
+			break;
+		case "-":
+			Sub sub = new Sub();
+			sub.setValue(a, b);
+			System.out.println(sub.calculate());
+			break;
+		case "*":
+			Mul mul = new Mul();
+			mul.setValue(a, b);
+			System.out.println(mul.calculate());
+			break;
+		case "/":
+			Div div = new Div();
+			div.setValue(a, b);
+			System.out.println(div.calculate());
+			break;
+		}
+	}
+
+}
