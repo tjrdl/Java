f27f86df232dec39f5db824e82f81533f3c05bfe
tjrdl <tjrdlchlrh00@gmail.com>
2025-02-25 10:20:18 +0900

    Random Array

---
+++ question08/RandomArray.java
@@ -0,0 +1,30 @@
+package Java.question08;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 몇개? ");
+		int amount = sc.nextInt();
+		Random r = new Random();
+
+		ArrayList<Integer> array = new ArrayList<>();
+
+		for (int i = 1; i < amount + 1; i++) {
+			int a = r.nextInt(100) + 1;
+			array.add(a);
+		}
+		for (int i = 1; i < amount + 1; i++) {
+			System.out.print(array.get(i) + "\t");
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+		}
+	}
+
+}
